edb76619da4636aaa9bf00ceb4fa6136c7ca6878
Kamil Dralewski <kamildralewski@interia.pl>
2024-01-01 07:35:39 +0100

    utworzenie plików index.html, style.css, main.js. W pliku stworzenie szkieletu slidera, a w pliku css zwykły prosty reset

---
+++ main.js
@@ -0,0 +1,7 @@
+const slides = [
+    'https://cdn.pixabay.com/photo/2021/09/28/14/39/horse-6664660_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/04/24/08/57/street-5085971_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/04/18/09/39/poppy-5058494_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2020/02/11/12/07/portofino-4839356_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2021/06/24/12/35/sunset-6361127_960_720.jpg'
+];
